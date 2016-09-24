439c183db1b9da56e29247884b311ea0366e8511
jaimielogan <jaimielogan@gmail.com>
2016-09-24 12:11:43 -0600

    Created knex.js file and updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
 // Update with your config settings.
+if(!process.env.NODE_ENV){
+  require('dotenv').config();
+}
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/gnarcar'
   },
 
   staging: {
@@ -26,12 +27,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
