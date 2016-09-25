3767c9e639b78733ca0d63a9b767afd6d1a3b625
Stephen Knutter <stephen.knutter@gmail.com>
2016-09-25 15:21:57 -0600

    setting up signup route

--- passport.js
+++ passport.js
@@ -1,3 +1,27 @@
 var passport = require("passport");
-var local = require("passport-local").Strategy;
-var 
+var Strategy = require("passport-local").Strategy;
+var users = require("./database/user");
+
+passport.use(new Strateg(function(username, password, done) {
+  users.findByUsername(username).then(function(user) {
+    user = user[0];
+    if (!user) done(null, false);
+
+    var check = users.authenticateUser(password, user.password);
+    if (!check) return done(null, false);
+
+    done(null, user);
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  users.findByUsername(username).then(function(user) {
+    user = user[0];
+    if (!user) return false;
+    done(null, user);
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,19 @@ router.get('/signup', function(req, res, next) {
   res.render('signup', {title: 'Sign Up'});
 });
 
+router.post('/signup', function(req, res, next) {
+  var formVars = req.body;
+
+  var username = formVars.username;
+  var email = formVars.email;
+  var password = formVars.password;
+  var confirm  = formVars.confirm;
+
+  if(!username || !email || !password || !confirm){
+    return false;
+  }
+
+  
+});
+
 module.exports = router;
