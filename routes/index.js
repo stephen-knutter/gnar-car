3ef02a217aaf1b2fb73eae379e2f70bb0bdcb452
Stephen Knutter <stephen.knutter@gmail.com>
2016-09-25 17:36:26 -0600

    updating auth

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express'});
-});
-
 router.get('/signup', function(req, res, next) {
   res.render('signup', {title: 'Sign Up'});
 });
@@ -21,8 +16,11 @@ router.post('/signup', function(req, res, next) {
   if(!username || !email || !password || !confirm){
     return false;
   }
+});
 
-  
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
