f75c8350d72ae842eb29d2d9dd18a49ffcf89676
jaimielogan <jaimielogan@gmail.com>
2016-09-27 15:09:48 -0600

    created knex query for limited knex

--- database/ride.js
+++ database/ride.js
@@ -9,6 +9,10 @@ function getRideMountainDriverData(){
   return knex('ride').select('ride.id AS rideID','ride.seats_avail', 'ride.cost_seat', 'ride.home_depTime','mountain.name','mountain.id AS mountainID','mountain.image_url','users.username').innerJoin('mountain','mountain.id','ride.mountain_id').innerJoin('car_user','car_user.car_id','ride.car_id').innerJoin('users','users.id','car_user.user_id');
 }
 
+function getRideDataLimitThree(){
+  return getRideMountainDriverData().orderBy('ride.home_depTime','desc').limit(3);
+}
+
 function getRideMountainDriverDataByMountainId(id){
   return getRideMountainDriverData().where('mountain_id',id);
 }
@@ -16,5 +20,6 @@ function getRideMountainDriverDataByMountainId(id){
 module.exports = {
   getRides: getRides,
   getRideMountainDriverData: getRideMountainDriverData,
+  getRideDataLimitThree: getRideDataLimitThree,
   getRideMountainDriverDataByMountainId: getRideMountainDriverDataByMountainId
 };
