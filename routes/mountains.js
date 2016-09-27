5d6a9f7ebad9b048384a1cc5e3d1eaeedeb4ed9d
Stephen Knutter <stephen.knutter@gmail.com>
2016-09-27 10:50:17 -0600

    fixing mountain and index page

--- routes/mountains.js
+++ routes/mountains.js
@@ -18,80 +18,6 @@ router.get('/:mountainId', function(req, res, next) {
   var weatherCodes = [];
   var allIcons;
   var weatherIcons = [];
-<<<<<<< HEAD
-
-  // mountain.findMountainsById(req.params.mountainId)
-  // .then(function(mountainInfo){
-    // request(mountainInfo[api_url], function(error, response, body)
-    //   if(!error & response.statusCode == 200){
-    //     parser.parseString(body, function(err,result){
-    //       // res.json({weatherData: result});
-    //       res.render('mountains', {mountainInfo: mountainInfo, weatherData:result});
-    //     });
-    //   }
-    // });
-  request('http://www.myweather2.com/developer/weather.ashx?uac=lWSw6eGT9t&uref=ee1b280c-f69d-45a2-8373-6e19cd428117', function(error, response, body) {
-    if (!error & response.statusCode === 200) {
-      parser.parseString(body, function(error, result) {
-        result = result.weather;
-        conditions = result.snow_report;
-        forecast = result.forecast;
-        for (var i = 0; i < forecast.length; i++) {
-          weatherCodes.push(Number(forecast[i].day[0].weather_code[0]));
-        }
-
-        weatherIcons = ['/images/weather_icons/Sunny.gif',
-            '/images/weather_icons/Sunny.gif',
-            '/images/weather_icons/Sunny.gif',
-            '/images/weather_icons/Sunny.gif',
-            '/images/weather_icons/PartCloudRainThunderDay.gif',
-            '/images/weather_icons/Sunny.gif'];
-
-        mountain.findMountainsById(req.params.mountainId)
-              .then(function(mountainInfo) {
-                console.log(mountainInfo);
-                api_url = mountainInfo[0].api_url;
-                request(api_url, function(error, response, body) {
-                  if (!error & response.statusCode === 200) {
-                    parser.parseString(body, function(error, result) {
-                      result = result.weather;
-                      conditions = result.snow_report;
-                      forecast = result.forecast;
-                      for (var i = 0; i < forecast.length; i++) {
-                        weatherCodes
-                          .push(Number(forecast[i].day[0].weather_code[0]));
-                      }
-                      // Need to make a mountain to get the associated weather Icon
-                      for (var j = 0; j < weatherCodes.length; j++) {
-                        mountain.getWeatherIcon(weatherCodes[j])
-                          .then(function(weatherIcon) {
-                            weatherIcons.push(weatherIcon);
-                          });
-                      }
-
-                      weatherIcons = ['/images/weather_icons/Sunny.gif',
-                        '/images/weather_icons/Sunny.gif',
-                        '/images/weather_icons/Sunny.gif',
-                        '/images/weather_icons/Sunny.gif',
-                        '/images/weather_icons/PartCloudRainThunderDay.gif',
-                        '/images/weather_icons/Sunny.gif'];
-
-                      for (var k = 0; k < forecast.length; k++) {
-                        forecast[k].weatherIcon = weatherIcons[k];
-                      }
-
-                      res.render('mountains',
-                        {mountainInfo: mountainInfo,
-                        weatherData: result,
-                        conditions: conditions,
-                        forecast: forecast});
-                    });
-                  }
-                });
-              });
-      });
-    }
-=======
   mountain.findMountainsById(req.params.mountainId)
   .then(function(mountainInfo){
     api_url = mountainInfo[0].api_url;
@@ -124,7 +50,6 @@ router.get('/:mountainId', function(req, res, next) {
         });
       }
     });
->>>>>>> jaimielogan-mountainDB
   });
 });
 
