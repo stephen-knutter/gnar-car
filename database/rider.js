7cf4c0ec9ad5493b007b77a353f3939d7d8577e2
jaimielogan <jaimielogan@gmail.com>
2016-09-27 22:42:16 -0600

    Created 'rides/:rideId', updated queries to populate, and updated views

--- database/rider.js
+++ database/rider.js
@@ -1,7 +1,7 @@
 var knex = require('./config');
 
 function findRidersByRideID(ride_id){
-  return knex('rider').where('ride_id',ride_id);
+  return knex('rider').innerJoin('users','users.id','rider.user_id').where('ride_id',ride_id);
 }
 
 function addRiderToRide(ride_id, user_id){
--- migrations/20160927155830_rider.js
+++ migrations/20160927155830_rider.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('rider', function(table){
     table.integer('ride_id').references('id').inTable('ride').onDelete('CASCADE');
-    table.integer('user_id').references('id').inTable('user').onDelete('CASCADE');
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
     table.unique(['ride_id', 'user_id']);
