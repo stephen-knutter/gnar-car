0a240f52bed757163add2f9fdc7d6e2967097c85
jaimielogan <jaimielogan@gmail.com>
2016-09-28 11:58:33 -0600

    Updated mountains router so temps are shown in deg f and created addRide query

--- database/ride.js
+++ database/ride.js
@@ -5,6 +5,12 @@ function getRides(){
   return knex('ride');
 }
 
+function addRide(car_id, mountain_id, departureDate, departureTime, returnDate, returnTime, seats_avail, cost_seat, meetup_loc){
+  var home_depTime = departureDate + departureTime;
+  var mtn_depTime = returnDate + returnTime;
+  return knex('ride').insert({car_id: car_id, mountain_id: mountain_id, home_depTime: home_depTime, mtn_depTime: mtn_depTime, seats_avail: seats_avail, cost_seat: cost_seat, meetup_loc: meetup_loc, pickup: false});
+}
+
 function getRideData(){
   return knex('ride').select('ride.id AS rideID','ride.seats_avail', 'ride.cost_seat', 'ride.home_depTime', 'ride.meetup_loc', 'mountain.name','mountain.id AS mountainID','mountain.image_url AS mountain_image','users.username','users.image_url').innerJoin('mountain','mountain.id','ride.mountain_id').innerJoin('car_user','car_user.car_id','ride.car_id').innerJoin('users','users.id','car_user.user_id').orderBy('ride.home_depTime','asc');
 }
@@ -31,6 +37,7 @@ function getDriverRatingByRideID(rideID){
 
 module.exports = {
   getRides: getRides,
+  addRide: addRide,
   getRideData: getRideData,
   getCarDataByRideID: getCarDataByRideID,
   getRideDataByRideID: getRideDataByRideID,
